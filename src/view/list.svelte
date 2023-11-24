43b2441225eb0d2a0ddd9bb0a7e2f6f20ca740b9
zxhd <zxhd863943427@outlook.com>
2023-11-25 01:05:45 +0800

    add list

---
+++ src/view/list.svelte
@@ -0,0 +1,11 @@
+<div>
+{@html html}
+</div>
+
+<script lang="ts">
+export let dataList:any[]
+import { lute } from "@/libs/utils";
+let trimList = dataList.map(x=>"* "+x.toString())
+let mdStr = trimList.join("\n")
+let html = lute.Md2BlockDOM(mdStr)
+</script>
