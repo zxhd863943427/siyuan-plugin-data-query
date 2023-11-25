ffda64014b1dd3061ac7d4e2f59fe2a210bdae96
zxhd <zxhd863943427@outlook.com>
2023-11-25 19:01:47 +0800

    init card table

---
+++ src/view/table.svelte
@@ -0,0 +1,17 @@
+<div class="b3-dialog__content">
+    <div>appId:</div>
+    <div class="fn__hr"></div>
+    <div class="plugin-sample__time">${app?.appId}</div>
+    <div class="fn__hr"></div>
+    <div class="fn__hr"></div>
+    <div>API demo:</div>
+    <div class="fn__hr" />
+    <div class="plugin-sample__time">
+        System current time: <span id="time">{time}</span>
+    </div>
+    <div class="fn__hr" />
+    <div class="fn__hr" />
+    <div>Protyle demo: id = {blockID}</div>
+    <div class="fn__hr" />
+    <div id="protyle" style="height: 360px;" bind:this={divProtyle}/>
+</div>
